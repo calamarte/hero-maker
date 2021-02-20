fe12dc1afd5096dd80b3303a74df72f6852566a3
calamarte <calamarte@gmail.com>
2021-02-20 08:45:21 +0100

    NEW: Initial commit

---
+++ src/test/java/es/atsistemas/heromaker/HeroMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.atsistemas.heromaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
