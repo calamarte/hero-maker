e7d34e729fa844c0dd1de8b716ee428a6f91cef8
calamarte <calamarte@gmail.com>
2021-02-21 12:55:32 +0100

    NEW: Dockerized

--- src/main/java/es/atsistemas/heromaker/config/PropertiesConfiguration.java
+++ src/main/java/es/atsistemas/heromaker/config/PropertiesConfiguration.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource({
-        "classpath:application.properties",
-        "classpath:database.properties",
-        "classpath:token.properties"
-})
+@PropertySource({"classpath:compiled.properties"})
 public class PropertiesConfiguration {
 }
